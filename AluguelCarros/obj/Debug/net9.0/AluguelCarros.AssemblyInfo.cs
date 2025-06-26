68e58cf0e84745449312315c9c8df1e94bc0f77a
LeoVPI <leonardomaycon2016@gmail.com>
2025-06-26 03:39:06 -0300

    Atualizei as configurações do json

--- AluguelCarros/obj/Debug/net9.0/AluguelCarros.AssemblyInfo.cs
+++ AluguelCarros/obj/Debug/net9.0/AluguelCarros.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AluguelCarros")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2550b7bad092fde5307c197d19d9eea0665fe328")]
 [assembly: System.Reflection.AssemblyProductAttribute("AluguelCarros")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AluguelCarros")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
